6bd43a08f685ffe152e55fe7e0bd2647a3486ff1
BOmokehinde <BOmokehinde@sis.tv>
2024-07-31 11:21:27 +0100

    add more tests for sum of positives

--- src/test/java/org/bukola/katas/sumOfPositives/MainSumOfPositivesTest.java
+++ src/test/java/org/bukola/katas/sumOfPositives/MainSumOfPositivesTest.java
@@ -1,12 +1,20 @@
 package org.bukola.katas.sumofpositives;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainSumOfPositivesTest {
 
-    @org.junit.jupiter.api.Test
-    void testSumOfPositives() {
-        int[] array = new int[]{-1, -5, 0, -10, 20, 30, 50};
-       assertEquals(100, MainSumOfPositives.sumOfPositiveNumbers(array));
+    @Test
+    void basic() {
+        int[] inputNumbers = new int[]{-1, -5, 0, -10, 20, 30, 50};
+       assertEquals(100, MainSumOfPositives.sumOfPositiveNumbers(inputNumbers));
+
+    }
+    @Test
+    void emptyInput () {
+        int[] inputNumbers = new int[]{};
+        assertEquals(0, MainSumOfPositives.sumOfPositiveNumbers(inputNumbers));
     }
 }
